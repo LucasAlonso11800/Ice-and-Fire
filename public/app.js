3adb9ddf10bfda6b402448591cee12e624db1e13
Lucas Alonso <lucasalonso11800@gmail.com>
2021-03-04 16:23:51 -0300

    Index and houses styling

--- public/app.js
+++ public/app.js
@@ -1,41 +1,9 @@
-const button = document.getElementById('button');
-const output = document.getElementById('output')
-const button2 = document.getElementById('button2');
-const output2 = document.getElementById('output2')
-const token = "goW_fCRLk3Al_d1z5DyN"
-const url = "https://the-one-api.dev/v2"
-let rofk;
-
-button.addEventListener('click', function(){
-    fetch(`${url}/movie/`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let result = '<h2>Works</h2>'
-        result += `<p>${data.docs[7].name}</p>`
-        output.innerHTML = result
-        rofk = data.docs[7]._id
-    })
-    .catch(err => {
-        console.log(err)
-    })
-});
-
-button2.addEventListener('click', function(){
-    fetch(`${url}/character?name=Boromir`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-});
+const targaryen = document.getElementById('targaryen');
+const stark = document.getElementById('stark');
+const arryn = document.getElementById('arryn');
+const tully = document.getElementById('tully');
+const greyjoy = document.getElementById('greyjoy');
+const lannister = document.getElementById('lannister');
+const baratheon = document.getElementById('baratheon');
+const tyrell = document.getElementById('tyrell');
+const martell = document.getElementById('martell');
--- server.js
+++ server.js
@@ -5,11 +5,15 @@ if(process.env.NODE_ENV !=="production"){
 const API_KEY = process.env.API_KEY;
 const axios = require('axios')
 const express = require('express');
+const path = require('path')
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+// API CALLS
+const url = "https://www.anapioficeandfire.com/api"
 app.post('/characters', (req, res) => {
 
 });
